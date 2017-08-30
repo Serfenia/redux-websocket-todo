121923c4296ab568a50cbb6cf3574e90676547c9
Patrick van Vuuren <patrick@proforto.nl>
2017-08-30 12:08:02 +0200

    Moved socket calls to actions

--- client/actions/todos.js
+++ client/actions/todos.js
@@ -1,4 +1,10 @@
+import socket from './../socket';
+import listeners from './../listeners/todos';
+
+listeners(socket);
+
 export function searchTodos(filters) {
+    socket.emit('todos/search', filters);
     return {type: 'SEARCH_TODOS', filters};
 }
 
@@ -7,10 +13,12 @@ export function refreshTodos(todos) {
 }
 
 export function saveTodo(todo) {
+    socket.emit('todos/create', todo);
     return {type: 'SAVE_TODO', todo};
 }
 
 export function deleteTodo(index) {
+    socket.emit('todos/destroy', index);
     return {type: 'DELETE_TODO', index};
 }
 
--- client/reducers/todos.js
+++ client/reducers/todos.js
@@ -1,23 +1,8 @@
-import socket from './../socket';
-import listeners from './../listeners/todos';
-
-listeners(socket);
-
 export function todos(state = [], action) {
     switch (action.type) {
-        case 'SEARCH_TODOS':
-            socket.emit('todos/search', action.filters);
-            // Search filters could be saved here.
-            break;
-        case 'SAVE_TODO':
-            socket.emit('todos/create', action.todo);
-            break;
-        case 'DELETE_TODO':
-            socket.emit('todos/destroy', action.index);
-            break;
         case 'REFRESH_TODOS':
             return action.todos;
             break;
+        default: return state;
     }
-    return state
 }
